029b6651c8459a847e7fc8b0275fe5e246a23500
James Tansiri <tansirijames@gmail.com>
2026-04-10 06:44:31 +0700

    feat: update sitemap base url

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,7 +1,7 @@
 module.exports = {
 	title: "Thalyn Diamond Chiang Mai",
 	description: "รับออกแบบเครื่องประดับทุกชนิดที่สะท้อนความเป็นคุณ",
-	url: "https://www.thalyndiamond.com",
+	url: "https://thalyndiamond.com",
 	language: "th",
 	author: {
 		name: "Data Advantage Co.,Ltd.",
